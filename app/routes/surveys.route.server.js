22e6bc983f4a9eca6237154abdc0e61b68db8bc3
AJ-CentennialCollege <abalbaca@my.centennialcollege.ca>
2022-11-12 20:02:14 -0500

    Authorization Completed

--- app/routes/surveys.route.server.js
+++ app/routes/surveys.route.server.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
-//import { AuthGuard } from "../utils/index.js";
+import { AuthGuard } from "../utils/index.js";
 import {DisplaySurveyList, DisplayCreateSurveyPage, ProcessSurveyCreatePage,DisplaySurveyEditPage, ProcessSurveyEditPage,
 DisplaySurveyPage, ProcessSurveyPage, DisplaySurveyStatsPage, ProcessSurveyDelete} from "../controllers/surveys.controller.server.js";
 
 const router = Router();
 
 router.get('/surveys/list', DisplaySurveyList);
-router.get('/surveys/create',DisplayCreateSurveyPage);
-router.post('/surveys/create', ProcessSurveyCreatePage);
-router.get('/surveys/edit/:id', DisplaySurveyEditPage);
-router.post('/surveys/edit/:id', ProcessSurveyEditPage);
-router.get('/surveys/view/:id', DisplaySurveyPage);
-router.post('/surveys/view/:id',ProcessSurveyPage);
-router.get('/surveys/stats/:id',DisplaySurveyStatsPage);
-router.get('/survey-delete/:id', ProcessSurveyDelete);
+router.get('/surveys/create', AuthGuard, DisplayCreateSurveyPage);
+router.post('/surveys/create', AuthGuard, ProcessSurveyCreatePage);
+router.get('/surveys/edit/:id', AuthGuard, DisplaySurveyEditPage);
+router.post('/surveys/edit/:id', AuthGuard, ProcessSurveyEditPage);
+router.get('/surveys/view/:id', AuthGuard, DisplaySurveyPage);
+router.post('/surveys/view/:id', AuthGuard, ProcessSurveyPage);
+router.get('/surveys/stats/:id', AuthGuard, DisplaySurveyStatsPage);
+router.get('/survey-delete/:id', AuthGuard, ProcessSurveyDelete);
 
 
 
