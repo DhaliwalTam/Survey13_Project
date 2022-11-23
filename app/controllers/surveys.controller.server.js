6986381803b2569f9ea8713906a09e19243ba14d
Siddharth Verma <vermasiddharth51@gmail.com>
2022-11-23 17:40:02 -0500

    Minor edit to survey controller

--- app/controllers/surveys.controller.server.js
+++ app/controllers/surveys.controller.server.js
@@ -37,7 +37,7 @@ export function DisplaySurveyList(req, res, next) {
                         id:GetUserID(req), 
                         username: GetUsername(req)
                     });
-                })
+                });
             }
         })
     } 
