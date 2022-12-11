67ea54b379622c6a197caf18ee7cd94fb3edc282
Siddharth Verma <vermasiddharth51@gmail.com>
2022-12-11 14:21:01 -0500

    Minor changes

--- app/controllers/surveys.controller.server.js
+++ app/controllers/surveys.controller.server.js
@@ -133,14 +133,6 @@ export function DisplaySurveyEditPage(req, res, next) {
     });
 }
 
-
-function arrayEquals(a, b) {
-    return Array.isArray(a) &&
-      Array.isArray(b) &&
-      a.length === b.length &&
-      a.every((val, index) => val === b[index]);
-  }
-
 // processes survey edit page/survey update page
 export function ProcessSurveyEditPage(req,res,next){
     var id = req.params.id;
